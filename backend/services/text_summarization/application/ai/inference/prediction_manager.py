86619a796ed729667d33226641a57fd7fae422f6
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2021-08-26 00:37:57 +0530

    setting up inference pipeline

--- backend/services/text_summarization/application/ai/inference/prediction_manager.py
+++ backend/services/text_summarization/application/ai/inference/prediction_manager.py
@@ -1,4 +1,5 @@
 from common.logging.console_loger import ConsoleLogger
+from services.text_summarization.application.ai.model import T5Model
 from services.text_summarization.application.ai.training.src.preprocess import Preprocess
 from services.text_summarization.settings import Settings
 
@@ -8,12 +9,27 @@ class PredictionManager:
         self.preprocess = preprocess
         self.logger = logger
         self.settings = Settings
-        self.__model = None
+        self.__t5_model = None
         self.__load_model()
 
     def __load_model(self):
         try:
             self.logger.info(message="Loading T5(text-to-text-transfer-transformer) Model for text extraction.")
+            self.__t5_model = T5Model(model_name=self.settings.MODEL_NAME,
+                                      model_type=self.settings.MODEL_TYPE)
+
+            self.__t5_model.load_model(model_type=self.settings.MODEL_TYPE,
+                                       use_gpu=self.settings.USE_GPU,
+                                       model_path=self.settings.WEIGHTS_PATH
+                                       )
+            self.logger.info(message="Model Weights loaded Successfully--!!")
 
         except BaseException as ex:
             self.logger.error(message="Exception Occurred while loading model---!! " + str(ex))
+
+    def __predict(self, data):
+        try:
+            self.logger.info(message="Performing prediction on the given data.")
+
+        except BaseException as ex:
+            self.logger.error(message="Exception Occurred while prediction---!! " + str(ex))
--- backend/services/text_summarization/application/configuration.py
+++ backend/services/text_summarization/application/configuration.py
@@ -1,10 +1,8 @@
 from injector import Module, singleton
 
 from common.logging.console_loger import ConsoleLogger
-# from common.logging.logger import Logger
-# from services.text_summarization.application.ai.inference.prediction_manager import PredictionManager
-# from services.text_summarization.application.ai.training.src.preprocess import Preprocess
-# from services.text_summarization.settings import Settings
+from services.text_summarization.application.ai.inference.prediction_manager import PredictionManager
+from services.text_summarization.application.ai.training.src.preprocess import Preprocess
 from services.text_summarization.settings import Settings
 
 
@@ -12,4 +10,4 @@ class Configuration(Module):
     def configure(self, binder):
         logger = ConsoleLogger(filename=Settings.LOGS_DIRECTORY)
         binder.bind(ConsoleLogger, to=logger, scope=singleton)
-        # binder.bind(PredictionManager, to=PredictionManager(preprocess=Preprocess(), logger=logger), scope=singleton)
+        binder.bind(PredictionManager, to=PredictionManager(preprocess=Preprocess(), logger=logger), scope=singleton)
